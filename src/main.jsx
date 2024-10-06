6f487a533b237fcda8d29c2815e1eee433e13e83
Smart-imran <rst.imran4@gmail.com>
2024-10-06 20:18:57 +0600

    route path about contact

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><h2>Hello World</h2></div>
+  },
+
+  {
+    path: '/about',
+    element: <div><h2>This is about page</h2></div>
+  },
+  {
+    path: '/contact',
+    element: <div><h2>This is contact page</h2></div>
+  },
+  
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
