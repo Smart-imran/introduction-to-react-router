4f4d80c218dd410891b49e8e9550eb91ed3cdefb
Smart-imran <rst.imran4@gmail.com>
2024-10-09 02:18:04 +0600

    LinkNav

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,27 @@
+import { useNavigate, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+
+    const error = useRouteError();
+    const navigate = useNavigate();
+    const hancldeClick = () =>{
+        navigate('/');
+    }
+
     return (
+        
         <div>
-            
+            <h2>opps </h2>
+            <p>{error.statusText || error.message}</p>
+            {
+                error.status === 404 && <div>
+
+                    <h3>Page Not Found</h3>
+                    <p>Go back where you from </p>
+                    <button onClick={hancldeClick}>Home</button>
+                </div>
+            }
         </div>
     );
 };
